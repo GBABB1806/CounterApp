d43fed9c98dc165395f7b6e048868a80dd9de5d6
GBABB1806 <gabriele.abbruscato06@gmail.com>
2023-12-03 16:36:45 +0100

    Aggiungere i file di progetto.

---
+++ CounterApp/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace CounterApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("Cheque-Regular.otf", "ChequeRegular");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
